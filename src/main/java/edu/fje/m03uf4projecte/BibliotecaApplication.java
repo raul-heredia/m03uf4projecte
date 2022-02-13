6dc057b0892aff5e0842865ee626629b13c68c41
Raúl Heredia <raul.heredia01@outlook.com>
2022-02-13 16:14:24 +0100

    Nada que comentar

--- src/main/java/edu/fje/m03uf4projecte/BibliotecaApplication.java
+++ src/main/java/edu/fje/m03uf4projecte/BibliotecaApplication.java
@@ -11,7 +11,7 @@ public class BibliotecaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BibliotecaApplication.class.getResource("biblioteca-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
